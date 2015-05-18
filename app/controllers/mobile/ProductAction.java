37d658e38cbf2def4bb713eff5d86089f44cd858
wangjiaming <jiaming.wang@sunlights.cc>
2015-05-18 09:25:06 +0800

    产品详情

---
+++ app/controllers/mobile/ProductAction.java
@@ -0,0 +1,21 @@
+package controllers.mobile;
+
+import controllers.BaseController;
+
+/**
+ * <p>Project: com.shovesoft.sp2p</p>
+ * <p>Title: ProductAction.java</p>
+ * <p>Description: </p>
+ * <p>Copyright (c) 2014 Sunlights.cc</p>
+ * <p>All Rights Reserved.</p>
+ *
+ * @author <a href="mailto:jiaming.wang@sunlights.cc">wangJiaMing</a>
+ */
+public class ProductAction extends BaseController {
+
+    public static void productDetail(){
+
+
+        render();
+    }
+}
