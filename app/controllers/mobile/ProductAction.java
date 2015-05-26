69a42967243318cd50aa4ec093e77dae7de55f3c
杨洪 <hong.yang@sunlights.cc>
2015-05-26 17:36:41 +0800

    add productdetail by yh

--- app/controllers/mobile/ProductAction.java
+++ app/controllers/mobile/ProductAction.java
@@ -4,6 +4,9 @@ import business.Bid;
 import controllers.BaseController;
 import play.Logger;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * <p>Project: com.shovesoft.sp2p</p>
  * <p>Title: ProductAction.java</p>
@@ -23,10 +26,27 @@ public class ProductAction extends BaseController {
         Logger.info(">>bidId:" + bidId);
         Bid bid = new Bid();
         bid.id = bidId;
+        Map jsonMap=new HashMap();
+        if(null!=bid.repayment_res && bid.repayment_res.length()>44){
+            try{
+                String project=bid.repayment_res.split(";")[0];
+                jsonMap.put("repayment_res_short",project.substring(0,44));//短的资金安全
+            }catch (Exception e) {
+                jsonMap.put("repayment_res_short",bid.repayment_res.substring(0,44));//短的资金安全
+            }
+        }else{
+            jsonMap.put("repayment_res_short",bid.repayment_res);
+        }
+
+        if(null!=bid.description && bid.description.length()>44){
+            jsonMap.put("project_introduction_short",bid.description.substring(0,44));
+        }else{
+            jsonMap.put("project_introduction_short",bid.description);
+        }
 
         Logger.info(">>current bid status:" + bid.status);
 
-        render(bid);
+        render(bid,jsonMap);
     }
     
     
